1ffcd1d2329d64f3246b18f21423fc5fe7a3ab6c
Tom E Flaherty <teflaherty@utexas.edu>
2023-06-17 22:49:49 -0500

    merged

--- ProjectCleanup/Utils/Utils.cs
+++ ProjectCleanup/Utils/Utils.cs
@@ -21,7 +21,6 @@ namespace ProjectCleanup
 
         #region Families
 
-
         public static List<Family> GetAllFamilies(Document doc)
         {
             List<Family> m_returnList = new List<Family>();
@@ -53,6 +52,49 @@ namespace ProjectCleanup
             }
 
             return m_returnList;
+        }
+
+        public static Family GetFamilyByName(Document doc, string familyName)
+        {
+            List<Family> famList = GetAllFamilies(doc);
+
+            foreach (Family curFam in famList)
+            {
+                if (curFam.Name == familyName)
+                    return curFam;
+            }
+
+            return null;
+        }
+
+        #endregion
+
+        #region Lines
+
+        internal static LinePatternElement GetLinePatternByName(Document curDoc, string typeName)
+        {
+            if (typeName != null)
+                return LinePatternElement.GetLinePatternElementByName(curDoc, typeName);
+            else
+                return null;
+        }
+
+        internal static GraphicsStyle GetLinestyleByName(Document curDoc, string styleName)
+        {
+            GraphicsStyle retlinestyle = null;
+
+            FilteredElementCollector gstylescollector = new FilteredElementCollector(curDoc);
+            gstylescollector.OfClass(typeof(GraphicsStyle));
+
+            foreach (Element element in gstylescollector)
+            {
+                GraphicsStyle curLS = element as GraphicsStyle;
+
+                if (curLS.Name == styleName)
+                    retlinestyle = curLS;
+            }
+
+            return retlinestyle;
         }
 
         #endregion
@@ -269,167 +311,6 @@ namespace ProjectCleanup
             return null;
         }
 
-
-        internal static List<ViewSchedule> GetScheduleByNameContains(Document doc, string scheduleString)
-        {
-            List<ViewSchedule> m_scheduleList = GetAllSchedules(doc);
-
-            List<ViewSchedule> m_returnList = new List<ViewSchedule>();
-
-            foreach (ViewSchedule curSchedule in m_scheduleList)
-            {
-                if (curSchedule.Name.Contains(scheduleString))
-                    m_returnList.Add(curSchedule);
-            }
-
-            return m_returnList;
-        }
-
-        #endregion
-
-        #region Sheets
-
-        internal static List<ViewSheet> GetAllSheets(Document doc)
-        {
-            //get all sheets
-            FilteredElementCollector m_colViews = new FilteredElementCollector(doc);
-            m_colViews.OfCategory(BuiltInCategory.OST_Sheets);
-
-            List<ViewSheet> m_sheets = new List<ViewSheet>();
-            foreach (ViewSheet x in m_colViews.ToElements())
-            {
-                m_sheets.Add(x);
-            }
-
-            return m_sheets;
-        }
-
-        #endregion
-
-        #region Parameters
-
-        public struct ParameterData
-        {
-            public Definition def;
-            public ElementBinding binding;
-            public string name;
-            public bool IsSharedStatusKnown;
-            public bool IsShared;
-            public string GUID;
-            public ElementId id;
-        }
-
-
-        internal static List<string> GetAllSheetGroupsByCategory(Document doc, string categoryValue)
-        {
-            List<string> m_groups = new List<string>();
-
-            // Get all sheet views in the project that have the specified category value
-            List<ViewSheet> m_sheets = GetAllSheetsByCategory(doc, categoryValue);
-
-            // Iterate through each sheet view and get the value of the "Group" parameter
-            foreach (ViewSheet sheet in m_sheets)
-            {
-                // Get the "Group" parameter of the sheet view
-                Parameter groupParameter = sheet.LookupParameter("Group");
-
-                // Check if the "Group" parameter is valid and get its value
-                if (groupParameter != null && groupParameter.Definition.Name == "Group")
-                {
-                    string groupValue = groupParameter.AsString();
-
-                    // Check if the group value is not null or empty, and if it hasn't already been added to the list
-                    if (!string.IsNullOrEmpty(groupValue) && !m_groups.Contains(groupValue))
-                    {
-                        m_groups.Add(groupValue);
-                    }
-                }
-            }
-
-            return m_groups;
-        }
-
-        #endregion
-
-
-        #region Views
-
-
-        internal static List<View> GetAllViews(Document doc)
-        {
-            {
-
-                FilteredElementCollector m_colviews = new FilteredElementCollector(doc);
-                m_colviews.OfCategory(BuiltInCategory.OST_Views);
-
-                List<View> m_views = new List<View>();
-                foreach (View x in m_colviews.ToElements())
-                {
-                    m_views.Add(x);
-                }
-
-                return m_views;
-            }
-        }
-
-        internal static List<View> GetAllViewsByCategory(Document doc, string catName)
-        {
-            List<View> m_colViews = GetAllViews(doc);
-
-            List<View> m_returnList = new List<View>();
-
-            foreach (View curView in m_colViews)
-            {
-                string viewCat = GetParameterValueByName(curView, "Category");
-
-                if (viewCat == catName)
-                    m_returnList.Add(curView);
-            }
-
-            return m_returnList;
-        }
-
-        internal static List<View> GetAllViewsByCategoryAndViewTemplate(Document doc, string catName, string vtName)
-        {
-            List<View> m_colViews = GetAllViewsByCategory(doc, catName);
-
-            List<View> m_returnList = new List<View>();
-
-            foreach (View curView in m_colViews)
-            {
-                ElementId vtId = curView.ViewTemplateId;
-
-                if (vtId != ElementId.InvalidElementId)
-                {
-                    View vt = doc.GetElement(vtId) as View;
-
-                    if (vt.Name == vtName)
-                        m_returnList.Add(curView);
-                }
-            }
-
-            return m_returnList;
-        }
-
-        #endregion
-
-        internal static T FindVisualChild<T>(DependencyObject parent) where T : DependencyObject
-        {
-            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
-            {
-                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
-                if (child != null && child is T)
-                    return (T)child;
-                else
-                {
-                    T foundChild = FindVisualChild<T>(child);
-                    if (foundChild != null)
-                        return foundChild;
-                }
-            }
-            return null;
-        }
-
         #endregion
 
         #region Schedules
@@ -486,6 +367,54 @@ namespace ProjectCleanup
 
             return m_sheets;
         }
+                
+        internal static List<string> GetAllSheetGroupsByCategory(Document doc, string categoryValue)
+        {
+            List<string> m_groups = new List<string>();
+
+            // Get all sheet views in the project that have the specified category value
+            List<ViewSheet> m_sheets = GetAllSheetsByCategory(doc, categoryValue);
+
+            // Iterate through each sheet view and get the value of the "Group" parameter
+            foreach (ViewSheet sheet in m_sheets)
+            {
+                // Get the "Group" parameter of the sheet view
+                Parameter groupParameter = sheet.LookupParameter("Group");
+
+                // Check if the "Group" parameter is valid and get its value
+                if (groupParameter != null && groupParameter.Definition.Name == "Group")
+                {
+                    string groupValue = groupParameter.AsString();
+
+                    // Check if the group value is not null or empty, and if it hasn't already been added to the list
+                    if (!string.IsNullOrEmpty(groupValue) && !m_groups.Contains(groupValue))
+                    {
+                        m_groups.Add(groupValue);
+                    }
+                }
+            }
+
+            return m_groups;
+        }
+
+        internal static List<ViewSheet> GetSheetsByGroupName(Document doc, string stringValue)
+        {
+            List<ViewSheet> m_viewSheets = GetAllSheets(doc);
+
+            List<ViewSheet> m_returnGroups = new List<ViewSheet>();
+
+            foreach (ViewSheet curSheet in m_viewSheets)
+            {
+                // Get the "Group" parameter of the sheet view
+                Parameter groupParameter = curSheet.LookupParameter("Group");
+
+                // Check for the "Group" parameter and add sheet tolist
+                if (groupParameter != null && groupParameter.AsValueString() == stringValue)
+                    m_returnGroups.Add(curSheet);
+            }
+
+            return m_returnGroups;
+        }
 
         public static List<ViewSheet> GetAllSheetsByCategory(Document doc, string categoryValue)
         {
@@ -516,54 +445,6 @@ namespace ProjectCleanup
             return m_sheets;
         }
 
-        internal static List<ViewSheet> GetSheetsByGroupName(Document doc, string stringValue)
-        {
-            List<ViewSheet> m_viewSheets = GetAllSheets(doc);
-
-            List<ViewSheet> m_returnGroups = new List<ViewSheet>();
-
-            foreach (ViewSheet curSheet in m_viewSheets)
-            {
-                // Get the "Group" parameter of the sheet view
-                Parameter groupParameter = curSheet.LookupParameter("Group");
-
-                // Check for the "Group" parameter and add sheet tolist
-                if (groupParameter != null && groupParameter.AsValueString() == stringValue)
-                    m_returnGroups.Add(curSheet);
-            }
-
-            return m_returnGroups;
-        }
-
-        internal static List<string> GetAllSheetGroupsByCategory(Document doc, string categoryValue)
-        {
-            List<string> m_groups = new List<string>();
-
-            // Get all sheet views in the project that have the specified category value
-            List<ViewSheet> m_sheets = GetAllSheetsByCategory(doc, categoryValue);
-
-            // Iterate through each sheet view and get the value of the "Group" parameter
-            foreach (ViewSheet sheet in m_sheets)
-            {
-                // Get the "Group" parameter of the sheet view
-                Parameter groupParameter = sheet.LookupParameter("Group");
-
-                // Check if the "Group" parameter is valid and get its value
-                if (groupParameter != null && groupParameter.Definition.Name == "Group")
-                {
-                    string groupValue = groupParameter.AsString();
-
-                    // Check if the group value is not null or empty, and if it hasn't already been added to the list
-                    if (!string.IsNullOrEmpty(groupValue) && !m_groups.Contains(groupValue))
-                    {
-                        m_groups.Add(groupValue);
-                    }
-                }
-            }
-
-            return m_groups;
-        }
-
         #endregion
 
         #region String
@@ -580,7 +461,7 @@ namespace ProjectCleanup
             return schedTitle.Length;
         }
 
-        #endregion
+        #endregion       
 
         #region Views
 
@@ -639,37 +520,7 @@ namespace ProjectCleanup
             return m_returnList;
         }
 
-        #endregion               
-
-        #region Lines
-
-        internal static LinePatternElement GetLinePatternByName(Document curDoc, string typeName)
-        {
-            if (typeName != null)
-                return LinePatternElement.GetLinePatternElementByName(curDoc, typeName);
-            else
-                return null;
-        }
-
-        internal static GraphicsStyle GetLinestyleByName(Document curDoc, string styleName)
-        {
-            GraphicsStyle retlinestyle = null;
-
-            FilteredElementCollector gstylescollector = new FilteredElementCollector(curDoc);
-            gstylescollector.OfClass(typeof(GraphicsStyle));
-
-            foreach (Element element in gstylescollector)
-            {
-                GraphicsStyle curLS = element as GraphicsStyle;
-
-                if (curLS.Name == styleName)
-                    retlinestyle = curLS;
-            }
-
-            return retlinestyle;
-        }
-
-        #endregion
+        #endregion           
 
         internal static T FindVisualChild<T>(DependencyObject parent) where T : DependencyObject
         {
